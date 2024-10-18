2bf3761e3c80c0ba5876072cea72d40265f1bbe6
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-10-18 21:46:07 +0000

    Setup for RPS

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("messages");
-const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+const choices = ["rock", "paper", "scissors"];
 
 /**
  * Add event listener to all the functions
@@ -29,7 +29,7 @@ function playGame(playerChoice) {
     playerImage.src = `assets/images/${choices[playerChoice]}.png`;
     playerImage.alt = choices[playerChoice];
 
-    let computerChoice = Math.floor(Math.random() * 5);
+    let computerChoice = Math.floor(Math.random() * 3);
 
     computerImage.src = `assets/images/${choices[computerChoice]}.png`;
     computerImage.alt = choices[computerChoice];
@@ -49,51 +49,19 @@ function playGame(playerChoice) {
 function checkWinner(computerChoice, playerChoice) {
     if (computerChoice == playerChoice) {
         return "draw"
-
     } else if (playerChoice == "rock" && computerChoice == "scissors") {
         return "player"
     } else if (playerChoice == "rock" && computerChoice == "paper") {
         return "computer"
-    } else if (playerChoice == "rock" && computerChoice == "lizard") {
-        return "player" 
-    } else if (playerChoice == "rock" && computerChoice == "spock") {
-        return "computer"
-
-    }  else if (playerChoice == "paper" && computerChoice == "rock") {
+    } else if (playerChoice == "paper" && computerChoice == "rock") {
         return "player"
     } else if (playerChoice == "paper" && computerChoice == "scissors") {
         return "computer"
-    }  else if (playerChoice == "paper" && computerChoice == "spock") {
-        return "player"
-    } else if (playerChoice == "paper" && computerChoice == "lizard") {
-        return "computer"
-
     } else if (playerChoice == "scissors" && computerChoice == "paper") {
         return "player"
     } else if (playerChoice == "scissors" && computerChoice == "rock") {
         return "computer"
-    } else if (playerChoice == "scissors" && computerChoice == "lizard") {
-        return "player"
-    } else if (playerChoice == "scissors" && computerChoice == "spock") {
-        return "computer"
-
-    } else if (playerChoice == "lizard" && computerChoice == "paper") {
-        return "player"
-    } else if (playerChoice == "lizard" && computerChoice == "rock") {
-        return "computer"
-    } else if (playerChoice == "lizard" && computerChoice == "spock") {
-        return "player"
-    } else if (playerChoice == "lizard" && computerChoice == "scissors") {
-        return "computer"
-
-    } else if (playerChoice == "spock" && computerChoice == "rock") {
-        return "player"
-    } else if (playerChoice == "spock" && computerChoice == "paper") {
-        return "computer"
-    } else if (playerChoice == "spock" && computerChoice == "scissors") {
-        return "player"
-    } else if (playerChoice == "spock" && computerChoice == "lizard") 
-        return "computer"
+    }
 }
 
 function updateMessage(result) {
@@ -121,24 +89,6 @@ function updateScores(result) {
 }
 
 
-function checkEndOfGame() {
-    if (playerScore) {
-      result = "end-player"
-    } else if (computerScore) {
-        result = "end-computer"
-   } 
-}
-
-function updateEndMessage() {
-    let endMessage 
-    if (result == "end-player") {
-        endMessage = "End of game - Player Wins" 
-    } else (result == "end-computer")
-        endMessage = "End of game - Computer Wins"
-
-    let endMessageDiv = document.getElementById("end-message");
-    endMessageDiv.innerHTML = endMessage;
-}
 
 
 
